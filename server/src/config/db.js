8a6957b4556cf6d6bb31de2c54543c46312540ba
sid1651 <siddharthnarva@gmail.com>
2026-01-03 22:58:47 +0530

    first commit

---
+++ server/src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDB=async ()=>{
+    mongoose.connection.on('connected',()=>{
+        console.log("MongoDB connected successfully");
+    })
+    mongoose.connection.on('error',(err)=>{
+        console.error("MongoDB connection error:",err);
+    });
+    try{
+        await mongoose.connect(process.env.MONGO_URI,{
+            dbName:''
+        })
+    }catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
